5d588c474fa4981d8e808b461396da36a64e2bf8
Kairos22 <alandiko84@gmail.com>
2022-08-27 09:27:01 +0200

    Completed ex-2

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ generatePaySlip(){
 }
 
 let emp1 = new Employee(1, "Basil", "Kulukundis", 234, 5550);
-let emp2 = new Employee(2, "Avalon", " ó Ceallaigh", 54, 4799);
+let emp2 = new Employee(2, "Avalon", "ó Ceallaigh", 54, 4799);
 let emp3 = new Employee(3, "Carlos", "Mavroleon", 007, 9999);
 
 emp1.generatePaySlip();
@@ -29,10 +29,38 @@ console.log('----------------------------1. Employee Classes');
 
 // 2. Manager Class
 class Manager extends Employee {
+   
     constructor(id, firstname, lastname, taxId, salary){
+        
         super (id, firstname, lastname, taxId, salary);
+        
+        this.managedEmployees = []
+      }
 
-    }
+      addManagedEmployee(employee) {
 
+        this.managedEmployees.push(employee)
+      }
 
+      removeManagedEmployee (employee) {
+        
+        const idx =this.managedEmployees.findIndex((item) => employee.i === item.i);
+
+        this.managedEmployees.splice(idx, 2);
+        console.log(this.managedEmployees);
+
+      }
 }
+
+const manager1 = new Manager(1, "Nicolas", "Mavroleon", 78695, 90000);
+const manager2 = new Manager(2, "Mark", "Heisenberg", 37550, 85000)
+// console.log(manager1);
+// manager1.generatePaySlip();
+manager1.addManagedEmployee(emp1);
+manager1.addManagedEmployee(emp2);
+// manager1.addManagedEmployee(emp3);
+console.log(manager1);
+manager2.addManagedEmployee(emp3);
+console.log(manager2);
+console.log('---------------------');
+manager1.removeManagedEmployee(emp2);
